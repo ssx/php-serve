59557adf218ce75942288e8e8ccacc8461c7d96f
Scott Wilcox <scott@dor.ky>
2014-12-09 21:54:04 +0000

    Adding shell tests

---
+++ tests/TestServe.php
@@ -0,0 +1,17 @@
+<?php
+class PHPServeTest extends PHPUnit_Framework_TestCase
+{
+    public function testServerHasRun()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function testServerCanConnect()
+    {
+        $this->assertEquals(1, 1);
+    }
+    public function testServerHasDied()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
